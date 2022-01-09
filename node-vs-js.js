dc9add234b9ff7ba98517d9aab6e93ecc629da44
yarentamir <yarden.tamir@walla.co.il>
2022-01-09 11:44:26 +0200

    first commit

---
+++ node-vs-js.js
@@ -0,0 +1,8 @@
+// ? What are 3 things node.js has that the client side javascript doesn’t have. And the reasons why.
+// ! answer:
+//1. web applications with real-time, two-way connections, where both the client and server can initiate communication, allowing them to exchange data freely.
+//2. allowing you to optimally reuse developer resources.
+//3. In Node.js, he gave developers a tool for working in the non-blocking, event-driven I/O paradigm
+// ? What are 3 things the client side javascript has that node.js doesn’t have. And the reasons why.
+// ! answer:
+console.log("hi");
