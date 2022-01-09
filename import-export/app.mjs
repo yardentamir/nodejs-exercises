ab882f52d9c0188eba7dac709ce7a7ba460e910d
yarentamir <yarden.tamir@walla.co.il>
2022-01-09 15:37:33 +0200

    import export done

---
+++ import-export/app.mjs
@@ -0,0 +1,6 @@
+// const { add, multiple, divide } = require("./utils.js");
+import { add, multiple, divide } from "./utils.js"; // change type of file from .js to .mjs to use import syntax
+
+console.log(add(4, 2));
+console.log(multiple(4, 2));
+console.log(divide(4, 2));
---
+++ import-export/import-export.js
@@ -0,0 +1,13 @@
+// ? What is the difference between import and require
+// ! answer:
+// One of the major differences between require() and import() is that require() can be called from anywhere inside the program whereas import() cannot be called
+// conditionally, it always runs at the beginning of the file.
+// The difference between import and require is require is for Node.JS, and import is a standard ECMAScript proposal.
+// import is an ECMAScript 6 feature, it hasn't yet been fully supported by Node.js.
+
+//? How can you enable using the import syntax using node js
+// ! answer:
+// Install necessary packages: "npm install @babel/core @babel/register @babel/preset-env --save-dev"
+//? Give 2 node.js environment variables that are not available when using the import syntax.
+// import fs from "fs";
+// import express from 'express';
