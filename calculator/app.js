2588826411678962894686673d5fd66df72edf8f
yarentamir <yarden.tamir@walla.co.il>
2022-01-10 11:10:55 +0200

    uninstall packges and yargs done

--- calculator/app.js
+++ calculator/app.js
@@ -1,10 +1,37 @@
-const chalk = require("chalk");
 const yargs = require("yargs");
 
 yargs.command({
   command: "add",
-  description: "number + number"
-  handler: function (argv) {
-    console.log("here")
-  }
+  description: "number + number",
+  handler(argv) {
+    console.log(argv._[1] + argv._[2]);
+    // console.log(argv.numbers[1] + argv.numbers[2]); // --numbers=4
+    // console.log(+process.argv[3] + +process.argv[4]);
+  },
 });
+
+yargs.command({
+  command: "sub",
+  description: "number - number",
+  handler(argv) {
+    console.log(argv._[1] - argv._[2]);
+  },
+});
+
+yargs.command({
+  command: "mult",
+  description: "number * number",
+  handler(argv) {
+    console.log(argv._[1] * argv._[2]);
+  },
+});
+
+yargs.command({
+  command: "pow",
+  description: "number ^ number",
+  handler(argv) {
+    console.log(Math.pow(argv._[1], argv._[2]));
+  },
+});
+
+yargs.parse();
